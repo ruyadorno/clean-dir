dd32abbbe0c335eb990e94a4f2d8885dced296d1
Ruy Adorno <ruyadorno@hotmail.com>
2015-02-08 19:59:46 -0500

    Added test for basic cleaning functionality

--- test/test.js
+++ test/test.js
@@ -1,11 +1,30 @@
-/*global describe, it */
+/*global describe, it, beforeEach, afterEach */
 'use strict';
+
 var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+
 var cleanDir = require('../');
 
-describe('clean-dir node module', function () {
-  it('must have at least one test', function () {
-    cleanDir();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+describe('clean-dir', function () {
+
+  beforeEach(function () {
+    fs.mkdirSync('.tmp');
+    fs.writeFileSync(path.join('.tmp', 'README.md'), '');
+    fs.writeFileSync(path.join('.tmp', 'package.json'), '{ name: "something" }');
+    fs.writeFileSync(path.join('.tmp', '.gitignore'), '');
   });
+
+  afterEach(function () {
+    fs.rmdirSync('.tmp');
+  });
+
+  it('should clean a directory from all files on it', function () {
+    assert(fs.readdirSync('.tmp').length, 3);
+    cleanDir();
+    assert(fs.readdirSync('.tmp').length, 0);
+  });
+
 });
+
